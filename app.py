25486091053e576ae7261ce3a7760d2ad0d55d3b
justheuristic <justheuristic@gmail.com>
2021-12-06 17:42:10 +0300

    lifehack

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ import streamlit as st
 from dashboard_utils.main_metrics import get_main_metrics
 
 st.set_page_config(page_title="Training Transformers Together - Mini-Dashboard", layout="wide")
-
+st.markdown("""<style>
+.reportview-container {
+    top: -90px;
+}
+</style>""", unsafe_allow_html=True)
 source = get_main_metrics()
 st.vega_lite_chart(
     source, {
