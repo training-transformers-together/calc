72f4abda111ea2d2579fb7366d3286dfd6ee83c8
justheuristic <justheuristic@gmail.com>
2021-12-06 04:31:28 +0300

    test with bokeh

--- app.py
+++ app.py
@@ -31,19 +31,6 @@ So, can individual researchers and small labs still train state-of-the-art? Yes 
 All it takes is for a bunch of us to come together. In fact, we're doing it right now and <b>you're invited to join!</b>
 """, vspace_before=12)
 
-#DEBUG
-import streamlit as st
-from bokeh.plotting import figure
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-p = figure(
-     title='simple line example',
-     x_axis_label='x',
-     y_axis_label='y')
-p.line(x, y, legend_label='Trend', line_width=2)
-st.bokeh_chart(p, use_container_width=True)
-#/DEBUG
-
 draw_current_progress()
 
 
